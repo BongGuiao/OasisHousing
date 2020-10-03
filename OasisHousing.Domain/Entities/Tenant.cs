38cb66051d02555f8e111ba272669e1757f7144d
bongguiao <bongguiao@gmail.com>
2020-10-03 08:41:57 +0800

    Add Domain Model - Tenant

---
+++ OasisHousing.Domain/Entities/Tenant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OasisHousing.Domain.Entities
+{
+    public class Tenant
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
